44797e43252b5477fb4d0c81161a984aae2387f7
imteajsajid12 <imteajsajid1@gmail.com>
2023-09-11 00:12:16 +0600

    day1

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$books =[
+    "imteaj sajid",
+    "sajid imteaj",
+    "imteaj imteaj"
+];
+
+?>
+<?php 
+
+
+echo $books[1];
+//{
+//    echo $book;
+//}
+
+?>
+<br>
+
+<?= $books[2] ?>
+</body>
+</html>
