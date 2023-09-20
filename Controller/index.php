f96fa1f1e6981aa77a6620d0044bcf25ae51474b
imteajsajid12 <imteajsajid1@gmail.com>
2023-09-20 20:26:27 +0600

    create a php route

---
+++ Controller/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$data = [
+    'name' => "Laracst",
+    'cost' => 59
+];
+require "view/index.view.php";
+
--- index.php
+++ index.php
@@ -1,20 +1,13 @@
 <?php
+$url = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-$data = [
-    'name' => "Laracst",
-    'cost' => 59
-];
-
-
-//conditions
-if ($data['cost'] > 99) {
-    echo "not intersted";
-}
-
-//create function
-function test()
+if ($url === '/')
 {
+    require ('./view/index.view.php');
 
 }
+if ($url === '/about') {
+   
+    require('./view/index.view.php');
+}
 
-require "view/index.view.php";
