51103b020039aed1a7c2492aba78c54e168327d4
imteajsajid12 <imteajsajid1@gmail.com>
2023-09-21 09:03:59 +0600

    add class

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
 <?php
-require "Route/web.php";
+//require "Route/web.php";
+// database connect/
+
+
+//create class
+class person{
+    public $name;
+    public $age;
+    public function berther(){
+     echo $this->name  ."imteaj";
+
+    }
+}
+$persn = new person();
+$persn->name= "imteaj";
+$persn->age= 23;
+$persn->berther();
