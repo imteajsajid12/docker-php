c68b741ba2a588bf1849b2be51bbc817c4acab10
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2023-09-30 13:05:39 +0600

    database

--- Database.php
+++ Database.php
@@ -1,19 +1,46 @@
 <?php
 
 // create a new database class
+class  Database
+{
+
+    public $connection;
+
+    public function __construct($username= 'user',$password= 'pass')
+    {
+//        $data = [
+//            'host' => 'db',
+//            'user' => 'user',
+//            'pass' => 'pass',
+//            'db' => 'database'
+//        ];
 
 
-class  Database {
 
 
-    public function __construct() {
+
+//        $den = 'mysql:' . http_build_query($data, '', ';');
+
+    //database connection
+//        $this ->connection = new PDO($den,$username,$password,$database,
+//        [
+//          PDO::ATTR_DEFAULT_FETCH_MODE => PDO:: FETCH_ASSOC
+//        ]);
+
+//        public function query( $query)
+//    {
+////        $statement = $this->connection->prepare($query);
+//        $statement->execute();
+//        return $statement->fetchAll(PDO::FETCH_ASSOC);
+//    }
+
+
 
     }
-
-    public function connect() {
-        PDO:: Connect();
-
-    }
-
 
 }
+//$db=new Database();
+//
+//$query="SELECT * FROM posts";
+//$result=$db->query($query);
+
--- index.php
+++ index.php
@@ -1,36 +1,20 @@
 <?php
-require "Route/web.php";
+//require "Route/web.php";
 // database connect/
 require ('Database.php');
 
 
-var_dump($_ENV =['DB_USERNAME']);
 
 
-
-//create class
-//class person{
-//    public $name;
-//    public $age;
-//    public function berther(){
-//     echo $this->name  ."imteaj";
-//
-//    }
+//$host = 'db';
+//$user = 'user';
+//$pass = 'pass';
+//$conn = new mysqli($host, $user, $pass);
+//if ($conn->connect_error) {
+//    die("Connection failed: " . $conn->connect_error);
+//} else {
+//    echo "Connected to MySQL server successfully!";
 //}
-//$persn = new person();
-//$persn->name= "imteaj";
-//$persn->age= 23;
-//$persn->berther();
-
-$host = 'db';
-$user = 'user';
-$pass = 'pass';
-$conn = new mysqli($host, $user, $pass);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected to MySQL server successfully!";
-}
 
 
 
